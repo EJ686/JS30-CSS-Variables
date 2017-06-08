3fe9908c330bcc8b57f700b904c3935e28c4b604
Edgar Juarez <ejjt.686@gmail.com>
2017-06-07 18:26:53 -0700

    Added event listeners for changes to value and mouse movements

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('test');
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+    console.log(this.value);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate,));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate,));
